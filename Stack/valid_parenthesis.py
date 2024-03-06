819bde0f1128d7f45656bda4ef3f43eea88b0259
arturo.hinojosa <arturo.hinojosa@bettercloud.com>
2024-03-05 23:08:03 -0600

    Changing logic of the problem

--- Stack/valid_parenthesis.py
+++ Stack/valid_parenthesis.py
@@ -5,28 +5,26 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 """
-
-
 def valid_parenthesis(s):
+
     hashmap = {
         ')': '(',
         '}': '{',
         ']': '['
     }
     stack = []
-    for c in s:
-        if c not in hashmap:
-            stack.append(c)
+
+    for p in s:
+        if p not in hashmap:
+            stack.append(p)
         else:
-            curr = hashmap[c]
-            if not stack or curr != stack[-1]:
+            curr = hashmap[p]
+            if not stack or stack[-1] != curr:
                 return False
             stack.pop()
 
     return True if not stack else False
 
 
-s = "]"
+s = "()"
 print(valid_parenthesis(s))
-
-# curr = (
